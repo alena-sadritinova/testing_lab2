6af14e347a9d8774f9567e6116faf35ac6b44563
alena-sadritinova <133566009+alena-sadritinova@users.noreply.github.com>
2023-05-19 19:58:59 +0700

    Add files via upload

---
+++ UnitTest21/Tests.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab2_test;
+using static lab2_test.Coordinates;
+
+namespace test1.Tests
+{
+
+    [TestClass()]
+    public class Tests
+    {
+
+        [TestMethod()]
+        public void VectorlenTest1()
+        {
+            //arrange
+            int x = 3;
+            int y = 4;
+            double expected = 5;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void VectorlenTest2()
+        {
+            //arrange
+            int x = -3;
+            int y = -4;
+            double expected = 5;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void VectorlenTest3()
+        {
+            //arrange
+            int x = 0;
+            int y = 4;
+            double expected = 4;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void VectorlenTest4()
+        {
+            //arrange
+            int x = 3;
+            int y = 0;
+            double expected = 3;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void VectorlenTest5()
+        {
+            //arrange
+            int x = -3;
+            int y = 0;
+            double expected = 3;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void VectorlenTest6()
+        {
+            //arrange
+            int x = 0;
+            int y = -5;
+            double expected = 5;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void VectorlenTest7()
+        {
+            //arrange
+            int x = 0;
+            int y = -3;
+            double expected = 3;
+            //act
+            Coordinates c = new Coordinates();
+            c.Init(x, y);
+            double actual = c.Vectorlen();
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void AddTest1()
+        {
+            //arrange
+            Coordinates expected = new Coordinates();
+            expected.Init(3, 5);
+            //act
+            Coordinates a = new Coordinates();
+            a.Init(1, 2);
+            Coordinates b = new Coordinates();
+            b.Init(2, 3);
+            Coordinates actual = new Coordinates();
+            actual.Init(0, 0);
+            actual = actual.Add(a, b);
+            //assert
+            Assert.IsTrue(expected.Equals(actual));
+        }
+        [TestMethod()]
+        public void AddTest2()
+        {
+            //arrange
+            Coordinates expected = new Coordinates();
+            expected.Init(-5, -3);
+            //act
+            Coordinates a = new Coordinates();
+            a.Init(-2, -1);
+            Coordinates b = new Coordinates();
+            b.Init(-3, -2);
+            Coordinates actual = new Coordinates();
+            actual.Init(0, 0);
+            actual = actual.Add(a, b);
+            //assert
+            Assert.IsTrue(expected.Equals(actual));
+        }
+        [TestMethod()]
+        public void AddTest3()
+        {
+            //arrange
+            Coordinates expected = new Coordinates();
+            expected.Init(0, 3);
+            //act
+            Coordinates a = new Coordinates();
+            a.Init(2, 6);
+            Coordinates b = new Coordinates();
+            b.Init(-2, -3);
+            Coordinates actual = new Coordinates();
+            actual.Init(0, 0);
+            actual = actual.Add(a, b);
+            //assert
+            Assert.IsTrue(expected.Equals(actual));
+        }
+    }
+}
---
+++ test1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2_test
+{
+    public class Coordinates
+    {
+        private double x;
+        private double y;
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public void Init(double a, double b)
+        {
+            x = a;
+            y = b;
+        }
+        public void Display()
+        {
+            string s, s1;
+            s1 = Convert.ToString(x);
+            s = "x:" + s1 + " ";
+            s1 = Convert.ToString(y);
+            s = s + "y:" + s1;
+            Console.WriteLine(s);
+        }
+        public void Read()
+        {
+            string s = "";
+            s = Console.ReadLine();
+            string[] s1;
+            s1 = s.Split(new char[] { ' ', '\t' },
+            StringSplitOptions.RemoveEmptyEntries);
+            x = Convert.ToInt32(s1[0]);
+            y = Convert.ToInt32(s1[1]);
+        }
+        public double Vectorlen()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public Coordinates Add(Coordinates a, Coordinates b)
+        {
+            Coordinates c = new Coordinates();
+            c.x = a.x + b.x;
+            c.y = a.y + b.y;
+            return c;
+        }
+        public override bool Equals(Object obj)
+        {
+            if (obj is Coordinates)
+            {
+                var that = obj as Coordinates;
+                return this.x == that.x && this.y == that.y;
+            }
+
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coordinates a = new Coordinates();
+            a.X = 10;
+            double k;
+            k = (int)a.X;
+            Console.WriteLine(k);
+            Coordinates b = new Coordinates();
+            b.Init(0, 10);
+            b.Display();
+            Coordinates c = new Coordinates();
+            c.Read();
+            c.Display();
+            k = b.Vectorlen();
+            Console.WriteLine(k);
+            Coordinates d = new Coordinates();
+            d = a.Add(b, c);
+            d.Display();
+        }
+    }
+}
+
+
+
